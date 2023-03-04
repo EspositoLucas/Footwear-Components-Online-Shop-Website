2b6161ede6556790da985a3675bd60d504990c1f
Lucas Esposito <79590470+EspositoLucas@users.noreply.github.com>
2023-03-04 14:52:54 -0300

    Update carrito.php

--- carrito.php
+++ carrito.php
@@ -33,10 +33,10 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
-                <!-- <a class="navbar-brand" href="./">Vida Informático</a> -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                 <a class="navbar-brand" href="./">Productos</a> 
+                <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">-->
+                <!--    <span class="navbar-toggler-icon"></span>-->
+                <!--</button>-->
             </div>
         </nav>
     </div>
